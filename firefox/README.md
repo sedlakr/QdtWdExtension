6491726e5eef20410c5fa7077aa8caac50e21037
Radek Sedlák <r.sedlak@quadient.com>
2025-02-27 09:49:11 +0100

    firefox

---
+++ firefox/README.md
@@ -0,0 +1,5 @@
+## How to install
+- Open Firefox
+- in URL enter about:debugging#/runtime/this-firefox
+- load temporary addon
+- select folder with this src
---
+++ firefox/content.js
@@ -0,0 +1,30 @@
+
+// content.js
+function convertTimeFormat(text) {
+  const regex = /Hours:\s*(\d+)(,(\d))?/g;
+  return text.replace(regex, (match, hours, decimals) => {
+    let minutes = Math.round((parseInt(decimals) / 10) * 60);
+	if(Number.isNaN(minutes)){
+		minutes=0;
+	}
+    return `${hours}h ${minutes}m`;
+  });
+}
+
+function updateTimes() {
+  document.querySelectorAll('*').forEach(node => {
+    if (node.childNodes.length === 1 && node.childNodes[0].nodeType === 3) {
+		debugger;
+      let newText = convertTimeFormat(node.textContent);
+      if (newText !== node.textContent) {
+        node.textContent = newText;
+      }
+    }
+  });
+}
+console.log("Running  QdtWdExtension");
+globalThis.updateTime = updateTimes;
+
+setInterval(()=>{
+	updateTimes();
+}, 1000)
