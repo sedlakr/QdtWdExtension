489d36f9d5400931e64f2069354e1dd5b8aabf70
Jan Černý <j.cerny@quadient.com>
2025-03-12 08:48:01 +0100

    feat(content.js): added time conversion for Summary section

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
 // content.js
-function convertTimeFormat(text) {
-  const regex = /Hours:\s*(\d+)([,.](\d+))?/g;
+function convertTimeFormat(text, isSummary = false) {
+  let regex;
+  if (!isSummary) {
+    regex = /^Hours:\s*(\d+)([,.](\d+))?/g
+  }
+  else {
+    regex = /^(-?\d+)([,.](\d+))?/g
+  }
+
+  if (/\d+h \d+m/.test(text) || /-\d+h \d+m/.test(text)) { 
+    return text; 
+  }
+
   return text.replace(regex, (_match, hours, _separator, decimals) => {
     let minutes = decimals ? Math.round((parseInt(decimals) / Math.pow(10, decimals.length)) * 60) : 0;
     return `${hours}h ${minutes}m`;
@@ -16,6 +27,14 @@ function updateTimes() {
       }
     }
   });
+
+  document.querySelectorAll('[data-automation-id="summarizedListItemValue"]').forEach(node => {
+    let newText = convertTimeFormat(node.textContent, true);
+      if (newText !== node.textContent) {
+        node.textContent = newText;
+      }
+  });
+
 }
 console.log("Running QdtWdExtension");
 globalThis.updateTime = updateTimes;
